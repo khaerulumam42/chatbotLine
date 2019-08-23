a6d8d3d215ce3c4612d42fead7e405655723a04f
Khaerul Umam <khaerulumam42@gmail.com>
2019-08-23 08:39:30 +0000

    first commit

--- app.py
+++ app.py
@@ -60,4 +60,5 @@ def handle_text_message(event):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host='0.0.0.0', port=port, ssl_context=("cert.pem", "key.pem"))
+    # app.run(host='127.0.0.1', port=port, ssl_context='adhoc')
