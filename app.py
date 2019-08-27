16c9d30439512e1be535dc8addc28ecdddbf590e
khaerulumam42 <umam@lenna.ai>
2019-08-27 21:26:13 +0700

    change ssl_context

--- app.py
+++ app.py
@@ -62,5 +62,4 @@ def handle_text_message(event):
 
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
-    app.run(debug=True, host='0.0.0.0', port=port, ssl_context=("cert.pem", "key.pem"))
-    # app.run(host='127.0.0.1', port=port, ssl_context='adhoc')
+    app.run(host='0.0.0.0', port=port, ssl_context='adhoc')
