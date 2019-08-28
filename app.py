c8ca77a5c95b4f5f33710fbb7fbcb6d6d58fac61
khaerulumam42 <umam@lenna.ai>
2019-08-28 15:17:30 +0700

    change port

--- app.py
+++ app.py
@@ -64,6 +64,6 @@ def handle_text_message(event):
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get('PORT', 5000))
+    port = 5000
     # app.run(host='0.0.0.0', port=port, ssl_context=('server.crt', 'server.key'))
     app.run(host='0.0.0.0', port=port)
