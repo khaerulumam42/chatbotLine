90dba5f243f9e0db62fc7f51d596d385d5120357
khaerulumam42 <umam@lenna.ai>
2019-10-03 14:10:48 +0700

    print data

--- utils_api.py
+++ utils_api.py
@@ -2,10 +2,12 @@ import requests
 
 def register_user(username, email, password, phone):
     data = {"name":username, "email":email, "password":password, "phone_no":phone}
+    print(data)
     reg = requests.post('http://18.223.160.194/Laundry/api/ebregister', files=data)
     return reg.json()
 
 def login_user(email, password):
+    print(email, password)
     data = {"email":email, "password":password}
     login = requests.post('http://18.223.160.194/Laundry/api/eblogin', files=data)
     return login.json()
